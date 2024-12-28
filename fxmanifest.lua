36c8a91393bf2b18b31eb1b9b8e3dc107746e981
DJ <136165865+ItzDeeJayy@users.noreply.github.com>
2024-12-28 15:56:00 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- FX Information --
+fx_version 'cerulean'
+game 'gta5'
+author 'DJ'
+description 'Collection Utensils'
+version '1.0.1'
+lua54 'yes'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/utensils.ytyp'
